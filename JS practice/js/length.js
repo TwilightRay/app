ba091e8c5570a7192e2e51e7c17c2bfdee0c6959
Victory <zaowik@gmail.com>
2020-03-02 22:49:21 +0600

    Доделать рекурсию на подсчет

--- JS practice/js/length.js
+++ JS practice/js/length.js
@@ -1,9 +1,8 @@
 'use strict'
+// Доделать
 function recLength(arr, n){
   n = n || 0;
-  if (arr==[]) return 0;
-  else {
-    arr.shift();
-    return recLength(arr, n++);}
+  if (arr==[]) return n;
+  return recLength(arr.splice(1, 1), n++);
 }
 alert(recLength([78, 'element', 244, 'item']));
