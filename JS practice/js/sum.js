fc40d26845fbdb099cd314415897468e95f0e7db
Victory <zaowik@gmail.com>
2020-03-02 13:01:57 +0600

    Доделана рекурсивная функция на сумму

--- JS practice/js/sum.js
+++ JS practice/js/sum.js
@@ -1,14 +1,14 @@
 'use strict'
-function sum(arr){
+function sumCycle(arr){
    let total = 0;
    for (let x in arr) total += arr[x];
    return total;
 }
-//alert(sum([5, 1, 2, 3, 4]))
+alert(sumCycle([5, 1, 2, 3, 4]))
 
-function sumRec(arr){
-  console.log(sumRec(arr));
-  if (arr == []) return 0;
-  return arr[0] + sumRec(arr.shift());
+function sumRec(arr, sum){
+  sum = sum || 0;
+  if (arr.length) return sumRec(arr, sum+arr.shift());
+  else return sum;
 }
 alert(sumRec([5, 1, 2, 3, 4]))
