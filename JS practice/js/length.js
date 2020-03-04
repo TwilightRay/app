63dd760e369d75fbd082fd2a126aadf9cd916a86
Victory <zaowik@gmail.com>
2020-03-04 13:06:29 -0500

    Recursive function for the Fibonacci formula completed

--- JS practice/js/length.js
+++ JS practice/js/length.js
@@ -1,8 +1,11 @@
 'use strict'
 // Доделать
+
 function recLength(arr, n){
-  n = n || 0;
-  if (arr==[]) return n;
-  return recLength(arr.splice(1, 1), n++);
+  console.log(arr);
+  if (arr===[]) return n;
+  return {
+    console.log(arr.splice(1,1));
+    recLength(arr.splice(1, 1), n++);}
 }
-alert(recLength([78, 'element', 244, 'item']));
+alert(recLength([78, 'element', 244, 'item'], 1));
