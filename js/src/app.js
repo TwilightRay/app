8ce6d51ab46cbc13dc8a76c57aee04c423ad986c
TwilightRay <zaowik@gmail.com>
2020-04-23 23:43:54 +0600

    auth

--- js/src/app.js
+++ js/src/app.js
@@ -1,12 +1,16 @@
 import {Question} from './question'
-import {isValid} from './utils'
+import {createModal, isValid} from './utils'
 import './styles.css'
+import {getAuthForm} from './auth'
 
 const form = document.getElementById('form')
+const modalBtn = document.getElementById('modal-btn')
 const input = form.querySelector('#question-input')
 const submitBtm = form.querySelector('#submit')
 
+window.addEventListener('load', Question.renderList)
 form.addEventListener('submit', submitFormHandker)
+modalBtn.addEventListener('click', openModal)
 input.addEventListener( 'input', () => {
   submitBtm.disabled = !isValid(input.value)
 })
@@ -28,4 +32,27 @@ function submitFormHandker(event) {
       submitBtm.disabled = false
     })
   }
+}
+
+
+function openModal() {
+  createModal('Авторизация', getAuthForm())
+  document
+  .getElementById('auth-form')
+  .addEventListener('submit', authFormHandler, {once: true})
+}
+
+function authFormHandler(event) {
+  event.preventDefault()
+
+    const email = event.target.querySelector('#email').value
+    const password = event.target.querySelector('#password').value
+
+    authWithEmailAndPassword(email, password)
+      .then(Question.fetch)
+      .then(renderModalAfterAuth)
+}
+
+function renderModalAfterAuth(content) {
+  console.log('Content', content);
 }
--- js/src/question.js
+++ js/src/question.js
@@ -13,14 +13,50 @@ export class Question {
       return question
     })
     .then(addToLocalStorage)
+    .then(Question.renderList)
+  }
+
+  static fetch(token) {
+    if (!token) {
+      return Promise.resolve('<p class="error">У вас нет токена</p>');
+    }
+    return fetch(`https://twilight-ray.firebaseio.com/questions.json?auth=${token}`)
+      .then( response => response.json())
+      .then( questions => {
+        console.log('Questions', questions);
+      })
+  }
+
+  static renderList() {
+    const questions = getQuestionsFromLocalStorage()
+
+    const html = questions.length
+      ? questions.map(toCard).join('')
+      : `<div class="mui--text-headline">Команд пока нет</div>`
+
+    const list = document.getElementById('list')
+
+    list.innerHTML = html
   }
 }
+
 function addToLocalStorage(question) {
-  const all = getQuestionsFromLocalStorage()
+  const all = getQuestionsFromLocalStorage();
   all.push(question)
-  localStorage.setItem('questions', JSON.stringify(question))
+  localStorage.setItem('questions', JSON.stringify(all))
 }
 
 function getQuestionsFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('questions') || '[]');
+  return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+  return `
+  <div class="mui--text-black-54">
+    ${new Date(question.date).toLocaleDateString()}
+    ${new Date(question.date).toLocaleTimeString()}
+  </div>
+  <div>${question.text}</div>
+  <br>
+  `
 }
