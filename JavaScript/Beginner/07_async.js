fbd6eb0da0eaf94dc9047358ec151d95ae56d9d3
TwilightRay <zaowik@gmail.com>
2020-06-29 20:40:09 +0600

    finish

---
+++ JavaScript/Beginner/07_async.js
@@ -0,0 +1,56 @@
+// Ассинхронность
+const timeout = setTimeout(() => {
+    console.log('After timeout')
+}, 100) // первый параметр ф-я callback, вторым количество миллисекунд
+// (делим на 1000 и получаем секунды)
+
+clearTimeout(timeout)
+
+const interval = setInterval(() => {
+    console.log('After interval')
+}, 1000) // через какой промежуток времени нужно запускать данный интервал
+
+clearInterval(interval)
+
+const delay = (callback, wait = 1000) => {
+    setTimeout(callback, wait)
+}
+delay(() => {
+    console.log('After 2 seconds')
+}, 1000)
+
+const delay1 = (wait = 1000) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, wait)
+    })
+    return promise
+}
+
+delay1(2500)
+    .then(() => {
+        console.log('After 2 seconds')
+    })
+    .catch(err => console.error('Error:', err)) // если произошла ошибка
+    .finally(() => console.log('Finally'))
+
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
+
+getData().then(data => console.log(data))
+
+async function asyncExample() {
+    try {
+        await delay1(3000) // ждем, когда выполнится строчка
+        const data = await getData() // await может применяться
+        // только к ассинхронным ф-ям. Получаем данные и записываем их в константу.
+        console.log('Data', data)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+asyncExample()
+
--- JavaScript/Beginner/app.js
+++ JavaScript/Beginner/app.js
@@ -1,47 +1,79 @@
-// Ассинхронность
-const timeout = setTimeout(() => {
-    console.log('After timeout')
-}, 100) // первый параметр ф-я callback, вторым количество миллисекунд
-// (делим на 1000 и получаем секунды)
+// Работа с DOM
+const heading = document.getElementById('hello')  // документ - объект,
+// представляющий дом-дерево
+// const heading2 = document.getElementsByTagName('h2')[0]
+// const heading2 = document.getElementsByClassName('h2-class')
+const heading2 = document.querySelector('h2') // css - по названия тега
+const heading21 = document.querySelector('.h2-class') // селекторы - по классу
+const heading22 = document.querySelector('#sub-hello') // возвращает всегда 1 эл-т
+const heading3 = heading2.nextElementSibling
+const h2List = document.querySelectorAll('h2')
 
-clearTimeout(timeout)
+console.log(h2List[1]) // получаем элемент из массива селектора
+console.log(heading3)
+console.log(heading21)
+console.log(heading22)
 
-const interval = setInterval(() => {
-    console.log('After interval')
-}, 1000) // через какой промежуток времени нужно запускать данный интервал
+console.dir(heading.textContent)
+console.dir(heading.id)
+console.log(heading)
 
-clearInterval(interval)
-
-const delay = (callback, wait = 1000) => {
-    setTimeout(callback, wait)
+styles = {
+    color: ['darkblue', 'darkyellow', 'darkgreen', 'darkred', 'darkorange'] ,
+    backgroundColor: ['lightblue', 'lightyellow', 'lightgreen', 'lightred', 'lightorange']
 }
-delay(() => {
-    console.log('After 2 seconds')
+
+function addStylesTo(node, text, color = 'darkblue', fontSize = '3rem') {
+    node.textContent = text
+    node.style.color = color
+    node.style.textAlign = 'center'
+    node.style.backgroundColor = 'lightblue'
+    node.style.padding = '1rem'
+    node.style.display = 'block'
+    node.style.width = '100%'
+    // falsy: ';, indefined, null, 0, false
+    if (fontSize) {
+        node.style.fontSize = fontSize
+    }
+}
+
+const link = heading3.querySelector('a')
+
+link.addEventListener('click', () => {
+    event.preventDefault() // отменяем переход по ссылке и оставляем только клик
+    console.log('click', event.target.getAttribute('href'))
+    const url = event.target.getAttribute('href')
+
+    window.location = url
+})
+
+setTimeout(() => {
+    addStylesTo(heading, 'New text for heading 1', 'darkred', '3rem')
 }, 1000)
+setTimeout(() => {
+    addStylesTo(heading2, 'Some text for head 2', 'darkorange', '2rem')
+}, 2000)
+setTimeout(() => {
+    addStylesTo(link, 'Another text for h2', 'darkgreen', '1.5rem')
+}, 3000)
 
-const delay1 = (wait = 1000) => {
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, wait)
-    })
-    return promise
+heading.onclick = () => {
+    if (heading.style.color !== 'darkred') {
+        heading.style.color = 'darkred'
+        heading.style.backgroundColor = 'tomato'
+    } else {
+        heading.style.color = 'darkorange'
+        heading.style.backgroundColor = 'lightyellow'
+    }
 }
 
-delay1(2500)
-    .then(() => {
-        console.log('After 2 seconds')
-    })
-    .catch(err => console.error('Error:', err)) // если произошла ошибка
-    .finally(() => console.log('Finally'))
+heading22.addEventListener('dblclick', () => {
+    if (heading21.style.color === 'darkorange') {
+        heading21.style.color = 'darkred'
+        heading21.style.backgroundColor = 'tomato'
+    } else {
+        heading21.style.color = 'darkorange'
+        heading21.style.backgroundColor = 'lightyellow'
+    }
+})
 
-const getData = () => new Promise(resolve => resolve([
-    1, 1, 2, 3, 5, 8, 13
-]))
-
-getData().then(data => console.log(data))
-
-async function asyncExample() {
-    await delay1(3000) // ждем, когда выполнится строчка
-    await getData() // await может применяться только к ассинхронным ф-ям
-}
